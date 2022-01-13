e2935507c4ecca6742df1d3dd3e292a55c1f5fc9
pixiake <guofeng@yunify.com>
2022-01-13 14:13:55 +0800

    add contorlplane group
    Signed-off-by: pixiake <guofeng@yunify.com>

--- apis/kubekey/v1alpha1/cluster_types.go
+++ apis/kubekey/v1alpha1/cluster_types.go
@@ -147,9 +147,10 @@ type HostCfg struct {
 
 // RoleGroups defines the grouping of role for hosts (etcd / master / worker).
 type RoleGroups struct {
-	Etcd   []string `yaml:"etcd" json:"etcd,omitempty"`
-	Master []string `yaml:"master" json:"master,omitempty"`
-	Worker []string `yaml:"worker" json:"worker,omitempty"`
+	Etcd         []string `yaml:"etcd" json:"etcd,omitempty"`
+	Master       []string `yaml:"master" json:"master,omitempty"`
+	ControlPlane []string `yaml:"controlPlane" json:"controlPlane,omitempty"`
+	Worker       []string `yaml:"worker" json:"worker,omitempty"`
 }
 
 // HostGroups defines the grouping of hosts for cluster (all / etcd / master / worker / k8s).
@@ -326,6 +327,17 @@ func (cfg *ClusterSpec) ParseRolesList(hostList map[string]string, logger *log.L
 			masterGroupList = append(masterGroupList, getHostsRange(host, hostList, "master", logger)...)
 		} else {
 			if err := hostVerify(hostList, host, "master"); err != nil {
+				logger.Fatal(err)
+			}
+			masterGroupList = append(masterGroupList, host)
+		}
+	}
+
+	for _, host := range cfg.RoleGroups.ControlPlane {
+		if strings.Contains(host, "[") && strings.Contains(host, "]") && strings.Contains(host, ":") {
+			masterGroupList = append(masterGroupList, getHostsRange(host, hostList, "controlPlane", logger)...)
+		} else {
+			if err := hostVerify(hostList, host, "controlPlane"); err != nil {
 				logger.Fatal(err)
 			}
 			masterGroupList = append(masterGroupList, host)
