bab0d3375a5acc2cbe8eec16ef8332da236d45bb
pixiake <guofeng@yunify.com>
2022-06-23 16:53:06 +0800

    support rsa-sha2-512
    Signed-off-by: pixiake <guofeng@yunify.com>

--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -150,7 +150,7 @@ func ParseCfg(clusterCfgPath, k8sVersion, ksVersion string, ksEnabled bool) (*ku
 // AllinoneCfg is used to generate cluster object for all-in-one mode.
 func AllinoneCfg(user *user.User, k8sVersion, ksVersion string, ksEnabled bool, logger *log.Logger) (*kubekeyapiv1alpha1.Cluster, string) {
 	allinoneCfg := kubekeyapiv1alpha1.Cluster{}
-	if output, err := exec.Command("/bin/sh", "-c", "if [ ! -f \"$HOME/.ssh/id_rsa\" ]; then ssh-keygen -t rsa -P \"\" -f $HOME/.ssh/id_rsa && ls $HOME/.ssh;fi;").CombinedOutput(); err != nil {
+	if output, err := exec.Command("/bin/sh", "-c", "if [ ! -f \"$HOME/.ssh/id_rsa\" ]; then ssh-keygen -t rsa-sha2-512 -P \"\" -f $HOME/.ssh/id_rsa && ls $HOME/.ssh;fi;").CombinedOutput(); err != nil {
 		log.Fatalf("Failed to generate public key: %v\n%s", err, string(output))
 	}
 	if output, err := exec.Command("/bin/sh", "-c", "echo \"\n$(cat $HOME/.ssh/id_rsa.pub)\" >> $HOME/.ssh/authorized_keys && awk ' !x[$0]++{print > \"'$HOME'/.ssh/authorized_keys\"}' $HOME/.ssh/authorized_keys").CombinedOutput(); err != nil {
